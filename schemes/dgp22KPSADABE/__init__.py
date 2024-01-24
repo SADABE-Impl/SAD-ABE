731ad857252dd9762c6e771b220ed88e79a76c32
SADABE-Impl <148085447+SADABE-Impl@users.noreply.github.com>
2024-01-24 12:56:05 +0800

    Add files via upload

--- schemes/dgp22KPSADABE/__init__.py
+++ schemes/dgp22KPSADABE/__init__.py
@@ -28,6 +28,40 @@ class DGP22KPSADABE(ABEnc):
         self.util = SecretUtil(groupObj, verbose=False)
         # self.util = MSP(self.group, verbose)
 
+    def getMatrixMinor(self, m,i,j):
+        return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+    def getMatrixDeternminant(self, m):
+        #base case for 2x2 matrix
+        if len(m) == 2:
+            return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+        determinant = 0
+        for c in range(len(m)):
+            determinant += ((-1)**c)*m[0][c]*self.getMatrixDeternminant(self.getMatrixMinor(m,0,c))
+        return determinant
+
+    def getMatrixInverseTranspose(self, m):
+        determinant = self.getMatrixDeternminant(m)
+        #special case for 2x2 matrix:
+        if len(m) == 2:
+            return [[m[1][1]/determinant, -1*m[0][1]/determinant],
+                    [-1*m[1][0]/determinant, m[0][0]/determinant]]
+
+        #find matrix of cofactors
+        cofactors = []
+        for r in range(len(m)):
+            cofactorRow = []
+            for c in range(len(m)):
+                minor = self.getMatrixMinor(m,r,c)
+                cofactorRow.append(((-1)**(r+c)) * self.getMatrixDeternminant(minor))
+            cofactors.append(cofactorRow)
+        #cofactors = self.transposeMatrix(cofactors)
+        for r in range(len((cofactors))):
+            for c in range(len((cofactors))):
+                cofactors[r][c] = cofactors[r][c]/determinant
+        return cofactors
+
     def setup(self):
         """
         Generates public key and master secret key.
@@ -36,42 +70,41 @@ class DGP22KPSADABE(ABEnc):
         if debug:
             print('Setup algorithm:\n')
 
-        # for simplity, we define B_{i,j} = 0 if i != j, B_{i,j} = b_i if i = j
-        #                         B^*_{i,j} = 0 if i != j, B_{i,j} = (b_i)^{-1} if i = j
         # B = B, B^* = Bi
         B = []
-        Bi = []
         for j1 in range(3):
-            x = []
-            y = []
+            y = [] 
             for j2 in range(3):
-                if j2 == j1:
-                    random = self.group.random(ZR)
-                    x.append(random)
-                    y.append(random ** (-1))
-                else:
-                    x.append(0)
-                    y.append(0)
-            B.append(x)
-            Bi.append(y)
+                random = self.group.random(ZR)
+                y.append(random)
+            B.append(y)
 
+        Bi = self.getMatrixInverseTranspose(B)
+
+        '''
+        test B,Bi
+        A2 = {}
+        for i in range(3):
+            y = []
+            for j1 in range(3):
+                sum = 0
+                for j2 in range(3):
+                    sum += B[j2][i] * Bi[j2][j1]
+                y.append(sum)
+            A2[i] = y
+        print(A2)
+        '''
 
         # D = D, D^* = Di
         D = []
-        Di = []
         for j1 in range(9):
-            x = []
-            y = []
+            y = [] 
             for j2 in range(9):
-                if j2 == j1:
-                    random = self.group.random(ZR)
-                    x.append(random)
-                    y.append(random ** (-1))
-                else:
-                    x.append(0)
-                    y.append(0)
-            D.append(x)
-            Di.append(y)
+                random = self.group.random(ZR)
+                y.append(random)
+            D.append(y)
+
+        Di = self.getMatrixInverseTranspose(D)
 
         # pick a random element from the two source groups and pair them
         g = self.group.random(G1)
@@ -84,29 +117,40 @@ class DGP22KPSADABE(ABEnc):
         for j1 in range(3):
             y = []
             for j2 in range(3):
-                y.append(g ** B[j1][j2])
+                y.append(g ** B[j2][j1])
             g_B.append(y)
 
         g_Bi = []
         for j1 in range(3):
             y = []
             for j2 in range(3):
-                y.append(h ** Bi[j1][j2])
+                y.append(h ** Bi[j2][j1])
             g_Bi.append(y)
 
         g_D = []
         for j1 in range(9):
             y = []
             for j2 in range(9):
-                y.append(g ** D[j1][j2])
+                y.append(g ** D[j2][j1])
             g_D.append(y)
 
         g_Di = []
         for j1 in range(9):
             y = []
             for j2 in range(9):
-                y.append(h ** Di[j1][j2])
+                y.append(h ** Di[j2][j1])
             g_Di.append(y)
+
+        '''
+        for i in range(9):
+            if i == 0:
+                y = []
+                for j1 in range(9):
+                    sum = 1
+                    for j2 in range(9):
+                        sum *= pair(g_D[i][j2], g_Di[j1][j2])
+                    print('prod',sum)
+        '''
 
         # the public key
         pk = {'g': g, 'g_B0': g_B[0], 'g_B2': g_B[2], 'g_Bi0': g_Bi[0], 'g_D0': g_D[0], 'g_D1': g_D[1], 'g_D2': g_D[2], 'g_Di0': g_Di[0], 'g_Di1': g_Di[1], 'g_Di2': g_Di[2], 'e_gh': e_gh}
@@ -143,40 +187,23 @@ class DGP22KPSADABE(ABEnc):
 
             # compute the first two components of k_\lambda^*
             pi = self.group.random(ZR)
-            # we make up the (msk['h'] ** 0) computation for the case that B, D are randomly chosen with B_{i,j} != 0
-            prod1 = pk['g_Di0'][0] ** pi 
-            prod2 = pk['g_Di1'][1] ** (pi * int(attr)) 
-            y.append(prod1)
-            y.append(prod2)
+            ri = self.group.random(ZR) 
 
-            # compute the third component
-            prod = pk['g_Di2'][2] ** shares[attr] 
-            y.append(prod)
-
-            # components 4-6 are 0
-            y.append(h0)
-            y.append(h0)
-            y.append(h0)
-
-            # component 7 depends on whether it is valid
-            if attr in semi_list:
-                prod = msk['g_Di6'][6] ** self.group.random(ZR) 
+            # transform (pi, pi * int(attr), shares[attr], 0, 0, 0, ri, 0, 0)_Di into the standard basis
+            for j in range(9):
+                if attr in semi_list:
+                    prod = (pk['g_Di0'][j] ** pi) * (pk['g_Di1'][j] ** (pi * int(attr))) * (pk['g_Di2'][j] ** shares[attr]) * (msk['g_Di6'][j] ** ri)
+                else:
+                    prod = (pk['g_Di0'][j] ** pi) * (pk['g_Di1'][j] ** (pi * int(attr))) * (pk['g_Di2'][j] ** shares[attr])
                 y.append(prod)
-            else:
-                y.append(h0)
-
-            # components 8-9 are 0
-            y.append(h0)
-            y.append(h0)
 
             K1[int(attr)] = y
                 
         # K_0^* = K0
         K0 = []
-        prod = pk['g_Bi0'][0] ** s 
-        K0.append(prod)
-        K0.append(h0)
-        K0.append(msk['g_Bi2'][2])
+        for j in range(3):
+            prod = (pk['g_Bi0'][j] ** s) * (msk['g_Bi2'][j])
+            K0.append(prod)
 
         return {'policy': policy, 'K_0': K0, 'K_1': K1}
 
@@ -199,31 +226,21 @@ class DGP22KPSADABE(ABEnc):
         for attr in attr_list:
             y = []
             sig = self.group.random(ZR)
-            # we make up the (pk['g'] ** 0) computation for the case that B, D are randomly chosen with B_{i,j} != 0
-            prod1 = pk['g_D0'][0] ** (sig * int(attr)) 
-            prod2 = pk['g_D1'][1] ** (-sig) * (pk['g'] ** 0) 
-            y.append(prod1)
-            y.append(prod2)
-            prod = pk['g_D2'][2] ** (w) 
-            y.append(prod)
-            y.append(g0)
-            y.append(g0)
-            y.append(g0)
-            if attr in semi_list:
-                prod = sk['g_D6'][6] ** self.group.random(ZR) 
+            ui = self.group.random(ZR)
+            # transform (sig * int(attr), -sig, w, 0, 0, 0, ui, 0, 0)_D into the standard basis
+            for j in range(9):
+                if attr in semi_list:
+                    prod = (pk['g_D0'][j] ** (sig * int(attr))) * (pk['g_D1'][j] ** (-sig) ) * (pk['g_D2'][j] ** w ) * (sk['g_D6'][6] ** ui)
+                else:
+                    prod = (pk['g_D0'][j] ** (sig * int(attr))) * (pk['g_D1'][j] ** (-sig) ) * (pk['g_D2'][j] ** w )
                 y.append(prod)
-            else:
-                y.append(g0)
-            y.append(g0)
-            y.append(g0)
+
             C1[int(attr)] = y
 
         C0 = []
-        prod = pk['g_B0'][0] ** w 
-        C0.append(prod)
-        C0.append(g0)
-        prod = pk['g_B2'][2] ** z 
-        C0.append(prod)
+        for j in range(3):
+            prod = (pk['g_B0'][j] ** w ) * (pk['g_B2'][j] ** z)
+            C0.append(prod)
 
         # compute C
         C = pk['e_gh'] ** z
